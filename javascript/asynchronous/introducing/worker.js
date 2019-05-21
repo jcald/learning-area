decb965657f2bb23cee94470c5ba6961096893b0
Jose Calderon-Celis <josecalderoncelis@gmail.com>
2019-05-21 12:01:21 -0500

    Introducction

--- javascript/asynchronous/introducing/worker.js
+++ javascript/asynchronous/introducing/worker.js
@@ -1,9 +1,10 @@
 onmessage = function() {
-  let myDate;
-  for(let i = 0; i < 10000000; i++) {
-    let date = new Date();
-    myDate = date
-  }
+    console.log('onmessage: Init');
+    let myDate;
+    for (let i = 0; i < 10000000; i++) {
+        let date = new Date();
+        myDate = date
+    }
 
-  postMessage(myDate);
+    postMessage(myDate);
 }
